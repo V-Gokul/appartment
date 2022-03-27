a4cf2a5ee57fec29d8d08c8b37f4cf434db2dd06
V-Gokul <gokulvettri@gmail.com>
2022-03-28 01:40:24 +0530

    samp

--- src/polls/polls.module.ts
+++
@@ -1,9 +0,0 @@
-// import { Module } from '@nestjs/common';
-// import { PollsService } from './polls.service';
-// // import { PollsResolver } from './polls.resolver';
-
-// @Module({
-//   providers: [PollsResolver, PollsService],
-//   exports: [PollsService],
-// })
-// export class PollsModule {}
